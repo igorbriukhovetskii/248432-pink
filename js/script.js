5d8a904bed2c91bb1a6fa9c6cfd50d602eab7035
igorbriukhovetskii <igorbruhoveckij@gmail.com>
2016-12-08 16:19:34 +0300

    Сверстал адаптивные сетки всех страниц для мобильной, планшетной и десктопной версий

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-var mainNavToggle = document.querySelector(".main-nav__toggle");
+var mainNavToggle = document.querySelector(".main-nav__menu-toggle");
 var mainNavItems = document.querySelector(".main-nav__items");
+
+mainNavToggle.classList.remove("main-nav__menu-toggle--no-js");
 mainNavItems.classList.remove("main-nav__items--no-js");
+mainNavItems.classList.toggle("main-nav__items--closed");
 
 function toggleMenu(event) {
   event.preventDefault();
   mainNavItems.classList.toggle("main-nav__items--closed");
-  mainNavToggle.classList.toggle("main-nav__toggle--close");
+  mainNavToggle.classList.toggle("main-nav__menu-toggle--close");
 }
 
-mainNavToggle.addEventListener("click", toggleMenu);
+  mainNavToggle.addEventListener("click", toggleMenu);
+
